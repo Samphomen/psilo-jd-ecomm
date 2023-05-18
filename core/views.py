8788a6e07a736cbab1d8cfe2b4b4ee2a0b1c59c3
Ishaq <ishaqcole@gmail.com>
2023-05-18 23:14:53 +0100

    first commit

---
+++ core/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
+from .models import Item, Order, OrderItem
+from django.utils import timezone
+
+# Create your views here.
+def products(request):
+    context = {
+        "items" : Item.objects.all()
+    }
+    return render(request, "products.html", context)
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(item=item)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+    return redirect("core:product", slug=slug)
